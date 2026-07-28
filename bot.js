7b8251ecb6acbbbf8294977a5a24903dddccaf36
xsamikx-creator <xsamikx@gmail.com>
2026-07-29 02:27:06 +0400

    Use versioned Mini App URL and Mushvig naming

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { Telegraf, Markup } from 'telegraf';
 
 const token = process.env.BOT_TOKEN;
-const appUrl = process.env.MINI_APP_URL || 'https://xsamikx-creator.github.io/Mishanya/';
+const appUrl = process.env.MINI_APP_URL || 'https://xsamikx-creator.github.io/Mishanya/?v=20260729-5';
 
 if (!token) {
   throw new Error('Не найден BOT_TOKEN. Добавьте токен бота в переменные окружения.');
@@ -11,20 +11,20 @@ if (!token) {
 const bot = new Telegraf(token);
 
 const launchKeyboard = Markup.inlineKeyboard([
-  Markup.button.webApp('🍻 Открыть выживание Мишани', appUrl)
+  Markup.button.webApp('🍻 Открыть выживание Мушвига', appUrl)
 ]);
 
 bot.start(async ctx => {
   await ctx.reply(
-    '🍻 Добро пожаловать в главное управление отпуском Мишани!\n\n' +
-    'Здесь считаются пиво, кальяны, шашлыки, поездки на море и общий уровень легендарности.\n\n' +
+    '🍻 Добро пожаловать в главное управление отпуском Мушвига!\n\n' +
+    'Здесь считаются пиво, кальяны, шашлыки, прогулки с Дамирчиком, поездки на море и общий уровень легендарности.\n\n' +
     'Нажимай кнопку ниже 👇',
     launchKeyboard
   );
 });
 
 bot.command('открыть', async ctx => {
-  await ctx.reply('Запускаем выживание Мишани 👇', launchKeyboard);
+  await ctx.reply('Запускаем выживание Мушвига 👇', launchKeyboard);
 });
 
 bot.command('помощь', async ctx => {
