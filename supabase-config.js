80f2fecebe5f2172c4390f770ec071dae5ad425e
xsamikx-creator <xsamikx@gmail.com>
2026-07-29 14:34:58 +0400

    Configure Supabase cloud gallery

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.MUSHVIG_SUPABASE = {
-  url: '',
-  anonKey: '',
+  url: 'https://hlmjwqjppzwlnwachpuh.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsbWp3cWpwcHp3bG53YWNocHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMTU5ODcsImV4cCI6MjEwMDg5MTk4N30.de4SLUKBhlRdvAK4WznlpMVh8knRUfbw_pTfF5VvPe0',
   bucket: 'mushvig-photos'
 };
